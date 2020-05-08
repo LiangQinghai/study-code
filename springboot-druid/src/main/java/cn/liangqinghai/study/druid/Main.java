c4204ed767d5e2584f9ce498d49aea6a1bd6944d
liangqinghai <liangqinghai@wakedata.com>
2020-05-08 19:25:21 +0800

    druid

---
+++ springboot-druid/src/main/java/cn/liangqinghai/study/druid/Main.java
@@ -0,0 +1,22 @@
+package cn.liangqinghai.study.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiangQinghai
+ * @Title Main
+ * @ProjectName study-code
+ * @Description
+ * @date 2020/5/8 19:24
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+
+    }
+
+}
